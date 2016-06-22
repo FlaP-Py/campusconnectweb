14f2197113addaec66bc595f71b820aa9a2a3157
Saurav Mehrotra <saurav24081996@gmail.com>
2016-06-22 14:59:34 +0530

    Added

--- campusconnect/wsgi.py
+++ campusconnect/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
+import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campusconnect.settings")
-
 application = get_wsgi_application()
+
