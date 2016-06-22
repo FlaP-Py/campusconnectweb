792fe606225b777804f9e290e469e0e2a2cf1d65
Saurav Mehrotra <saurav24081996@gmail.com>
2016-06-22 15:53:28 +0530

    Added

--- campusconnect/wsgi.py
+++ campusconnect/wsgi.py
@@ -11,7 +11,9 @@ import os, sys
 
 import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campusconnect.settings")
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
